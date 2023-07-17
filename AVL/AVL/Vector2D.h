830cc4c787a70daa51cdb6f2316489c84beb9b49
Heitor Hellou <heitor.hellou@outlook.com>
2023-07-17 19:34:30 -0300

    vector class

---
+++ AVL/AVL/Vector2D.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+
+namespace avl
+{
+	template <typename T>
+	class Vector2D
+	{
+	public:
+		T _x;
+		T _y;
+	public:
+		Vector2D();
+		Vector2D(T x, T y) : _x{ x }, _y{ y } { }
+	public:
+		friend std::ostream& operator<<(std::ostream& out, const Vector2D<T>& v) { out << "(" << v._x << ", " << v._y << ")"; return out; };
+	}; // class Vector2D
+
+	typedef Vector2D<uint32_t> Vector2u;
+	typedef Vector2D<int> Vector2i;
+	typedef Vector2D<float> Vector2f;
+} // namespace avl
