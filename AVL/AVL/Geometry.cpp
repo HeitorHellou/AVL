cf3d770efb4350baceb2e76c41e50757efaaac26
Heitor Hellou <heitor.hellou@outlook.com>
2023-08-05 13:17:18 -0300

    Geometry cleanup

--- AVL/AVL/Geometry.cpp
+++ AVL/AVL/Geometry.cpp
@@ -13,7 +13,13 @@ void Geometry::Draw(int32_t x, int32_t y, std::unique_ptr<sf::RenderWindow>& tar
 	sf::Vertex pixel(sf::Vector2f(x, y), color);
 	target->draw(&pixel, 1, sf::Points);
 }
-void Geometry::Draw(const sf::Vector2i& pos, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) {}
+
+void Geometry::Draw(const sf::Vector2i& pos, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
+{
+	sf::Vertex pixel(sf::Vector2f(pos.x, pos.y), color);
+	target->draw(&pixel, 1, sf::Points);
+}
+
 void Geometry::DrawLine(int32_t x1, int32_t y1, int32_t x2, int32_t y2, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
 {
 	sf::Vertex line[] =
@@ -28,7 +34,22 @@ void Geometry::DrawLine(int32_t x1, int32_t y1, int32_t x2, int32_t y2, std::uni
 
 	target->draw(lineVertices);
 }
-void Geometry::DrawLine(const sf::Vector2i& pos1, const sf::Vector2i& pos2, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) {}
+
+void Geometry::DrawLine(const sf::Vector2i& pos1, const sf::Vector2i& pos2, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
+{
+	sf::Vertex line[] =
+	{
+		sf::Vertex(sf::Vector2f(pos1.x, pos1.y), color),
+		sf::Vertex(sf::Vector2f(pos2.x, pos2.y), color)
+	};
+
+	sf::VertexArray lineVertices(sf::Lines, 2);
+	lineVertices[0] = line[0];
+	lineVertices[1] = line[1];
+
+	target->draw(lineVertices);
+}
+
 void Geometry::DrawCircle(int32_t x, int32_t y, int32_t radius, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
 {
 	sf::CircleShape circle;
@@ -40,7 +61,19 @@ void Geometry::DrawCircle(int32_t x, int32_t y, int32_t radius, std::unique_ptr<
 
 	target->draw(circle);
 }
-void Geometry::DrawCircle(const sf::Vector2i& pos, int32_t radius, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) {}
+
+void Geometry::DrawCircle(const sf::Vector2i& pos, int32_t radius, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
+{
+	sf::CircleShape circle;
+	circle.setPosition(pos.x, pos.y);
+	circle.setRadius(radius);
+	circle.setFillColor(sf::Color::Transparent);
+	circle.setOutlineThickness(2.0f);
+	circle.setOutlineColor(color);
+
+	target->draw(circle);
+}
+
 void Geometry::FillCircle(int32_t x, int32_t y, int32_t radius, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White)
 {
 	sf::CircleShape circle;
@@ -50,7 +83,17 @@ void Geometry::FillCircle(int32_t x, int32_t y, int32_t radius, std::unique_ptr<
 
 	target->draw(circle);
 }
-void Geometry::FillCircle(const sf::Vector2i& pos, int32_t radius, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) {}
+
+void Geometry::FillCircle(const sf::Vector2i& pos, int32_t radius, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
+{
+	sf::CircleShape circle;
+	circle.setPosition(pos.x, pos.y);
+	circle.setRadius(radius);
+	circle.setFillColor(color);
+
+	target->draw(circle);
+}
+
 void Geometry::DrawRect(int32_t x, int32_t y, int32_t w, int32_t h, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
 {
 	sf::RectangleShape rect(sf::Vector2f(w, h));
@@ -61,7 +104,18 @@ void Geometry::DrawRect(int32_t x, int32_t y, int32_t w, int32_t h, std::unique_
 
 	target->draw(rect);
 }
-void Geometry::DrawRect(const sf::Vector2i& pos, int32_t w, int32_t h, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) {}
+
+void Geometry::DrawRect(const sf::Vector2i& pos, int32_t w, int32_t h, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
+{
+	sf::RectangleShape rect(sf::Vector2f(w, h));
+	rect.setPosition(pos.x, pos.y);
+	rect.setFillColor(sf::Color::Transparent);
+	rect.setOutlineThickness(2.0f);
+	rect.setOutlineColor(color);
+
+	target->draw(rect);
+}
+
 void Geometry::FillRect(int32_t x, int32_t y, int32_t w, int32_t h, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
 {
 	sf::RectangleShape rect(sf::Vector2f(w, h));
@@ -71,7 +125,17 @@ void Geometry::FillRect(int32_t x, int32_t y, int32_t w, int32_t h, std::unique_
 
 	target->draw(rect);
 }
-void Geometry::FillRect(const sf::Vector2i& pos, int32_t w, int32_t h, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) {}
+
+void Geometry::FillRect(const sf::Vector2i& pos, int32_t w, int32_t h, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
+{
+	sf::RectangleShape rect(sf::Vector2f(w, h));
+	rect.setPosition(pos.x, pos.y);
+	rect.setFillColor(color);
+	rect.setOutlineThickness(0.0f);
+
+	target->draw(rect);
+}
+
 void Geometry::DrawTriangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
 {
 	sf::VertexArray lines(sf::Lines);
@@ -85,7 +149,20 @@ void Geometry::DrawTriangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int3
 
 	target->draw(lines);
 }
-void Geometry::DrawTriangle(const sf::Vector2i& pos1, const sf::Vector2i& pos2, const sf::Vector2i& pos3, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) {}
+void Geometry::DrawTriangle(const sf::Vector2i& pos1, const sf::Vector2i& pos2, const sf::Vector2i& pos3, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
+{
+	sf::VertexArray lines(sf::Lines);
+
+	lines.append(sf::Vertex(sf::Vector2f(pos1.x, pos1.y), color));
+	lines.append(sf::Vertex(sf::Vector2f(pos2.x, pos2.y), color));
+	lines.append(sf::Vertex(sf::Vector2f(pos2.x, pos2.y), color));
+	lines.append(sf::Vertex(sf::Vector2f(pos3.x, pos3.y), color));
+	lines.append(sf::Vertex(sf::Vector2f(pos3.x, pos3.y), color));
+	lines.append(sf::Vertex(sf::Vector2f(pos1.x, pos1.y), color));
+
+	target->draw(lines);
+}
+
 void Geometry::FillTriangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int32_t x3, int32_t y3, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
 {
 	sf::ConvexShape triangle;
@@ -99,6 +176,20 @@ void Geometry::FillTriangle(int32_t x1, int32_t y1, int32_t x2, int32_t y2, int3
 
 	target->draw(triangle);
 }
-void Geometry::FillTriangle(const sf::Vector2i& pos1, const sf::Vector2i& pos2, const sf::Vector2i& pos3, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) {}
+
+void Geometry::FillTriangle(const sf::Vector2i& pos1, const sf::Vector2i& pos2, const sf::Vector2i& pos3, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White) 
+{
+	sf::ConvexShape triangle;
+
+	triangle.setPointCount(3);
+	triangle.setPoint(0, sf::Vector2f(pos1.x, pos1.y));
+	triangle.setPoint(1, sf::Vector2f(pos2.x, pos2.y));
+	triangle.setPoint(2, sf::Vector2f(pos3.x, pos3.y));
+
+	triangle.setFillColor(color);
+
+	target->draw(triangle);
+}
+
 void Geometry::DrawString(int32_t x1, int32_t y1, const std::string& text, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White, uint32_t scale = 1) {}
 void Geometry::DrawString(const sf::Vector2i& pos, std::unique_ptr<sf::RenderWindow>& target, const sf::Color& color = sf::Color::White, uint32_t scale = 1) {}
--- AVL/AVL/Vector2D.h
+++ AVL/AVL/Vector2D.h
@@ -11,24 +11,24 @@ namespace avl
 		T _x;
 		T _y;
 	public:
-		Vector2D();
+		Vector2D() : _x{ 0 }, _y{ 0 } {}
 		Vector2D(T x, T y) : _x{ x }, _y{ y } { }
 	public:
 		friend std::ostream& operator<<(std::ostream& out, const Vector2D<T>& v) { out << "(" << v._x << ", " << v._y << ")"; return out; };
-		friend Vector2D operator+(const Vector2d<T>& other) { var x = _x + other._x; var y = _y + other._y; return new Vector2D(x, y); };
-		friend Vector2D operator-(const Vector2d<T>& other) { var x = _x - other._x; var y = _y - other._y; return new Vector2D(x, y); };
-		friend Vector2D operator/(const Vector2d<T>& other) { var x = other._x / _x; var y = other._y / _y; return new Vector2D(x, y);};
-		friend Vector2D operator*(const Vector2d<T>& other) { return Vector2D(_x * other._x, _y * other._y); };
-		friend Vector2D operator>(const Vector2d<T>& other) { double magnitude1 = std::sqrt(_x * _x + _y * _y); double magnitude2 = std::sqrt(other._x * other._x + other._y * other._y); return magnitude1 < magnitude2; };
-		friend Vector2D operator<(const Vector2d<T>& other) { double magnitude1 = std::sqrt(_x * _x + _y * _y); double magnitude2 = std::sqrt(other._x * other._x + other._y * other._y); return magnitude1 > magnitude2; };
-		friend Vector2D operator>=(const Vector2d<T>& other) { return !(*this < other); };
-		friend Vector2D operator<=(const Vector2d<T>& other) { return !(*this > other); };
-		friend Vector2D operator+=(const Vector2d<T>& other) { _x += other._x; _y += other._y; return *this; };
-		friend Vector2D operator-=(const Vector2d<T>& other) { _x -= other._x; _y -= other._y; return *this; };
-		friend Vector2D operator*=(const Vector2d<T>& other) { _x *= other._x; _y *= other._y; return *this; };
-		friend Vector2D operator==(const Vector2d<T>& other) { return std::fabs(_x - other._x) < 1e-6 && std::fabs(_y - other._y) < 1e-6; };
-		friend Vector2D operator!=(const Vector2d<T>& other) { return !(*this == other); };
-		friend Vector2D operator!==(const Vector2d<T>& other) { return !(*this == other); };
+		friend Vector2D operator+(const Vector2D<T>& other) { var x = _x + other._x; var y = _y + other._y; return new Vector2D(x, y); };
+		friend Vector2D operator-(const Vector2D<T>& other) { var x = _x - other._x; var y = _y - other._y; return new Vector2D(x, y); };
+		friend Vector2D operator/(const Vector2D<T>& other) { var x = other._x / _x; var y = other._y / _y; return new Vector2D(x, y);};
+		friend Vector2D operator*(const Vector2D<T>& other) { return Vector2D(_x * other._x, _y * other._y); };
+		friend Vector2D operator>(const Vector2D<T>& other) { double magnitude1 = std::sqrt(_x * _x + _y * _y); double magnitude2 = std::sqrt(other._x * other._x + other._y * other._y); return magnitude1 < magnitude2; };
+		friend Vector2D operator<(const Vector2D<T>& other) { double magnitude1 = std::sqrt(_x * _x + _y * _y); double magnitude2 = std::sqrt(other._x * other._x + other._y * other._y); return magnitude1 > magnitude2; };
+		friend Vector2D operator>=(const Vector2D<T>& other) { return !(*this < other); };
+		friend Vector2D operator<=(const Vector2D<T>& other) { return !(*this > other); };
+		friend Vector2D operator+=(const Vector2D<T>& other) { _x += other._x; _y += other._y; return *this; };
+		friend Vector2D operator-=(const Vector2D<T>& other) { _x -= other._x; _y -= other._y; return *this; };
+		friend Vector2D operator*=(const Vector2D<T>& other) { _x *= other._x; _y *= other._y; return *this; };
+		friend Vector2D operator==(const Vector2D<T>& other) { return std::fabs(_x - other._x) < 1e-6 && std::fabs(_y - other._y) < 1e-6; };
+		friend Vector2D operator!=(const Vector2D<T>& other) { return !(*this == other); };
+		friend Vector2D operator!==(const Vector2D<T>& other) { return !(*this == other); };
 	}; // class Vector2D
 
 	typedef Vector2D<uint32_t> Vector2u;
