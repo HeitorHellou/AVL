e92ad3785a1d2950c293ee6799c49cd2e5eab2f2
MarcoPagno <marcoapg@live.com>
2023-08-07 18:32:14 -0300

    ajustes de erro (verificar)

--- AVL/AVL/Vector2D.h
+++ AVL/AVL/Vector2D.h
@@ -11,27 +11,28 @@ namespace avl
 		T _x;
 		T _y;
 	public:
-		Vector2D() : _x{ 0 }, _y{ 0 } {}
+		Vector2D();
 		Vector2D(T x, T y) : _x{ x }, _y{ y } { }
 	public:
-		friend std::ostream& operator<<(std::ostream& out, const Vector2D<T>& v) { out << "(" << v._x << ", " << v._y << ")"; return out; };
-		friend Vector2D operator+(const Vector2D<T>& other) { var x = _x + other._x; var y = _y + other._y; return new Vector2D(x, y); };
-		friend Vector2D operator-(const Vector2D<T>& other) { var x = _x - other._x; var y = _y - other._y; return new Vector2D(x, y); };
-		friend Vector2D operator/(const Vector2D<T>& other) { var x = other._x / _x; var y = other._y / _y; return new Vector2D(x, y);};
-		friend Vector2D operator*(const Vector2D<T>& other) { return Vector2D(_x * other._x, _y * other._y); };
-		friend Vector2D operator>(const Vector2D<T>& other) { double magnitude1 = std::sqrt(_x * _x + _y * _y); double magnitude2 = std::sqrt(other._x * other._x + other._y * other._y); return magnitude1 < magnitude2; };
-		friend Vector2D operator<(const Vector2D<T>& other) { double magnitude1 = std::sqrt(_x * _x + _y * _y); double magnitude2 = std::sqrt(other._x * other._x + other._y * other._y); return magnitude1 > magnitude2; };
-		friend Vector2D operator>=(const Vector2D<T>& other) { return !(*this < other); };
-		friend Vector2D operator<=(const Vector2D<T>& other) { return !(*this > other); };
-		friend Vector2D operator+=(const Vector2D<T>& other) { _x += other._x; _y += other._y; return *this; };
-		friend Vector2D operator-=(const Vector2D<T>& other) { _x -= other._x; _y -= other._y; return *this; };
-		friend Vector2D operator*=(const Vector2D<T>& other) { _x *= other._x; _y *= other._y; return *this; };
-		friend Vector2D operator==(const Vector2D<T>& other) { return std::fabs(_x - other._x) < 1e-6 && std::fabs(_y - other._y) < 1e-6; };
-		friend Vector2D operator!=(const Vector2D<T>& other) { return !(*this == other); };
-		friend Vector2D operator!==(const Vector2D<T>& other) { return !(*this == other); };
+		friend std::ostream& operator<<(std::ostream& out, const Vector2D<T>& v) { out << "(" << v._x << ", " << v._y << ")"; return out; }
+		friend Vector2D operator+(const Vector2D<T>& other) { return Vector2D(_x + other._x, _y + other._y); }
+		friend Vector2D operator-(const Vector2D<T>& other) { return Vector2D(_x - other._x, _y - other._y); }
+		friend Vector2D operator/(const Vector2D<T>& lhs, const Vector2D<T>& rhs) { return Vector2D(lhs._x / rhs._x, lhs._y / rhs._y); }
+		friend Vector2D operator*(const Vector2D<T>& lhs, const Vector2D<T>& rhs) { return Vector2D(lhs._x * rhs._x, lhs._y * rhs._y); }
+		friend bool operator>(const Vector2D<T>& lhs, const Vector2D<T>& rhs) { double magnitude1 = std::sqrt(lhs._x * lhs._x + lhs._y * lhs._y); double magnitude2 = std::sqrt(rhs._x * rhs._x + rhs._y * rhs._y); return magnitude1 > magnitude2; }
+		friend bool operator<(const Vector2D<T>& lhs, const Vector2D<T>& rhs) { double magnitude1 = std::sqrt(lhs._x * lhs._x + lhs._y * lhs._y); double magnitude2 = std::sqrt(rhs._x * rhs._x + rhs._y * rhs._y); return magnitude1 < magnitude2; }
+		friend bool operator>=(const Vector2D<T>& lhs, const Vector2D<T>& rhs) { return !(lhs < rhs); }
+		friend bool operator<=(const Vector2D<T>& lhs, const Vector2D<T>& rhs) { return !(lhs > rhs); }
+		Vector2D& operator+=(const Vector2D<T>& other) { _x += other._x; _y += other._y; return *this; }
+		Vector2D& operator-=(const Vector2D<T>& other) { _x -= other._x; _y -= other._y; return *this; }
+		Vector2D& operator*=(const Vector2D<T>& other) { _x *= other._x; _y *= other._y; return *this; }
+		friend bool operator==(const Vector2D<T>& lhs, const Vector2D<T>& rhs) { return std::fabs(lhs._x - rhs._x) < 1e-6 && std::fabs(lhs._y - rhs._y) < 1e-6; }
+		friend bool operator!=(const Vector2D<T>& lhs, const Vector2D<T>& rhs) { return !(lhs == rhs); }
 	}; // class Vector2D
 
 	typedef Vector2D<uint32_t> Vector2u;
 	typedef Vector2D<int32_t> Vector2i;
 	typedef Vector2D<float> Vector2f;
+	template<typename T>
+	inline Vector2D<T>::Vector2D() {	}
 } // namespace avl
